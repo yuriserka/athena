41399fbf83086fd0be024cb9ac4df5f8d1d97d08
Thales Menezes <thalesmenezes13@gmail.com>
2019-12-01 22:05:57 -0300

    Rotas criadas

--- front/src/main.js
+++ front/src/main.js
@@ -1,15 +1,36 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueMaterial from 'vue-material'
+import Vue from 'vue';
+import VueMaterial from 'vue-material';
 import Buefy from 'buefy';
 
+import App from './App.vue';
+import ConfigStorage from './ConfigStorage.vue';
+import NotFound from './NotFound.vue';
+import Help from './Help.vue';
+
 import 'buefy/dist/buefy.css';
-import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/vue-material.min.css';
 
 Vue.use(Buefy);
-Vue.use(VueMaterial)
-Vue.config.productionTip = false
+Vue.use(VueMaterial);
+Vue.config.productionTip = false;
+
+const routes = {
+    '/': App,
+    '/help': Help,
+    '/config': ConfigStorage
+};
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    el: '#app',
+    data: {
+        currentRoute: window.location.pathname
+    },
+    computed: {
+        ViewComponent() {
+            return routes[this.currentRoute] || NotFound;
+        }
+    },
+    render(h) {
+        return h(this.ViewComponent);
+    }
+});
